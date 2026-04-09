af1ee1ddaee181fe806ce89627a7618655f169a6
Jayesh Kotkar <117332855+Jayeshkotkar@users.noreply.github.com>
2026-04-09 14:14:59 +0530

    Update db.js
    change the DB url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://jayeshkotkar01:CfqSfJTxEeao1i4N@cluster0.x7ithtz.mongodb.net/AirBnb')
+        // await mongoose.connect('mongodb+srv://jayeshkotkar01:CfqSfJTxEeao1i4N@cluster0.x7ithtz.mongodb.net/AirBnb')
+        await mongoose.connect('mongodb+srv://jayeshkotkar01:0ZVNCXY8XDQMuw33@cluster0.otkpcos.mongodb.net/Nestify')
         .then(()=>{
             console.log("MongoDB connected");
         })
